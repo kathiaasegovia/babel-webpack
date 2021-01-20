431e31efb3ae39ae708113330fd8876ee2ae687e
KathiaSegovia <kathiasegovia@gmail.com>
2021-01-19 23:21:39 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import './styles/styles.scss'
-//const template = <h1>@babel/preset-env Rules!</h1>
 
 const App = ()=> {
-  return <h1>React app setup with Babel, Webpack Serve & Sass!
-  </h1>
+  return <h1>React app setup with Babel, Webpack & Sass!</h1>
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
