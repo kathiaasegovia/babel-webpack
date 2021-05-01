002c3306e6d491bb790d5543cc84f6a49c8d4844
Kathia A. Segovia <76544794+kathiaasegovia@users.noreply.github.com>
2021-05-01 17:29:33 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles/styles.scss'
 
 const App = ()=> {
-  return <h1>React app setup with Babel, Webpack & Sass!</h1>
+  return <h1> Setup </h1>
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
